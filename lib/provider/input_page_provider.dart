00fba06f9e6e91781cf6201f6ed57377fc23942a
Chandra <ardhyachandrax@gmail.com>
2023-08-17 11:55:32 +0700

    add bmi function

--- lib/provider/input_page_provider.dart
+++ lib/provider/input_page_provider.dart
@@ -6,6 +6,7 @@ class InputPageProvider extends ChangeNotifier {
   int _height = 180;
   int _weight = 74;
   int _age = 19;
+  late double _bmi;
 
   GenderState get selectedGender => _selectedGender;
   int get height => _height;
@@ -30,5 +31,30 @@ class InputPageProvider extends ChangeNotifier {
   void updateAge(int newAge) {
     _age = newAge;
     notifyListeners();
+  }
+
+  String calculateBMI() {
+    _bmi = _weight / ((_height / 100) * (_height / 100));
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+
+  String getInterpretation() {
+    if (_bmi >= 25) {
+      return 'You have a higher than normal body weight. Try to exercise more.';
+    } else if (_bmi >= 18.5) {
+      return 'You have a normal body weight. Good job!';
+    } else {
+      return 'You have a lower than normal body weight. You can eat a bit more.';
+    }
   }
 }
